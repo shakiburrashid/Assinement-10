a5cdc969e5c324d7478b56e9ac7c10bd9226d935
shakiburrashid <shakiburrashid7@gmail.com>
2025-10-23 21:01:15 +0600

    footer section working

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -15,23 +15,13 @@ const Hero = () => {
         <div>
             <Swiper
                 modules={[Navigation, Pagination, Autoplay]}
-                // spaceBetween={10}
+                spaceBetween={10}
                 slidesPerView={1}
                 navigation={false}
-                // breakpoints={{
-                //     640: {
-                //         slidesPerView: 1,
-                //     },
-                //     768: {
-                //         slidesPerView: 2,
-                //     },
-                //     1024: {
-                //         slidesPerView: 3,
-                //     },
-                // }}
+    
                 pagination={{ clickable: true }}
                 loop={true}
-                autoplay={{ delay: 2000 }}
+                autoplay={{ delay: 2500, disableOnInteraction: false }}
                 className='w-300 h-150  mt-15 rounded-xl max-md:w-100 max-md:h-55 m-auto mb-20'
             >
                 <SwiperSlide className=''><img className='m-auto' src="https://i.ibb.co.com/931DCw4m/3.png" alt="Hero Image 1" /></SwiperSlide>

287f1ce74e4a4db814bd86ffb7881e0cd0553abc
shakiburrashid <shakiburrashid7@gmail.com>
2025-10-23 13:58:13 +0600

    book session is responsive in mobile size

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -32,16 +32,16 @@ const Home = () => {
       </div>
       <div className=''>
         <ToastContainer />
-        <div className='w-full h-110 mt-15 flex flex-col items-center justify-center p-10 bg-linear-to-r from-blue-500 to-purple-600'>
+        <div className='w-full h-110 mt-15 flex flex-col items-center justify-center p-10 bg-linear-to-r from-blue-500 to-purple-600 max-md:w-full  '>
           <h1 className='text-center font-extrabold text-5xl'>Book Session</h1>
           <form onSubmit={submitData} className='m-auto place-content-center'>
             <fieldset className="fieldset flex flex-col gap-4">
-              <label className="label text-xl">Name</label>
-              <input type="text" className="input w-200 h-15 text-xl" placeholder="Name" name='name' required />
-              <label className="label text-xl">Email</label>
-              <input type="email" className="input w-200 h-15 text-xl" placeholder="Enter your email" name='email' required  />
+              <label className="label text-xl max-md:text-sm text-white font-bold">Name</label>
+              <input type="text" className="input w-200 h-15 text-xl max-md:w-80 max-md:h-10 max-md:text-" placeholder="Name" name='name' required />
+              <label className="label text-xl max-md:text-sm text-white font-bold">Email</label>
+              <input type="email" className="input w-200 h-15 text-xl max-md:w-80 max-md:h-10 max-md:text-" placeholder="Enter your email" name='email' required  />
             </fieldset>
-            <button className="btn btn-neutral mt-4 w-50 h-15 text-xl">Login</button>
+            <button className="btn btn-neutral mt-4 w-50 h-15 text-xl max-md:h-10 max-md:w-25">Login</button>
           </form>
         </div>
       </div>

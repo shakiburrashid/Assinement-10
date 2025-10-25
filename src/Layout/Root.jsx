0898d4470c47832ecc9a80eef6acca39bf1fde05
shakiburrashid <shakiburrashid7@gmail.com>
2025-10-25 11:26:49 +0600

    User info edit and responsive

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from '../Components/Footer'
-import Hero from '../Components/Hero'
-import Collection_Details from '../Pages/Collection_Details'
-import UserInfo from '../Pages/UserInfo'
 import Loader from '../Pages/Loader'
-import ForgetPassword from '../Pages/ForgetPassword'
-import AnimateCard from '../Pages/AnimateCard'
+
 
 const Root = () => {
   const { state } = useNavigate()
@@ -16,7 +12,6 @@ const Root = () => {
       <Navbar />
       {state == 'loading' ? <Loader /> : <Outlet />}
       <Footer />
-      {/* <AnimateCard/> */}
       
     </>
   )
--- src/Pages/UserInfo.jsx
+++ src/Pages/UserInfo.jsx
@@ -3,18 +3,22 @@ import { AuthContext } from '../Context/AuthProvider';
 import { Link } from 'react-router-dom';
 
 const UserInfo = () => {
-  const {user} = use(AuthContext);
+  const { user } = use(AuthContext);
   return (
-    <div className='flex flex-col items-center h-150'>
-      <h1 className='text-center mt-20 font-bold text-4xl '>Your Profile</h1>
-      <div className='flex items-center h-50 justify-center gap-10 max-md:flex-col max-md:items-center max-md:justify-center max-md:mt-20'>
-        <div>
-          <img className='rounded-full w-30 h-30' src={user && user?.photoURL} alt="" />
-        </div>
-        <div className='space-y-4 max-md:text-center'>
-          <p>Name: {user && user?.displayName}</p>
-          <p>Email: {user && user?.email}</p>
-        <Link to={'/userinfo/update'} className='btn'>Update Profile</Link>
+    <div className='h-150 place-content-center'>
+      <div className='flex flex-col items-center  bg-gray-100 md:w-150 max-md:h-110 max-md:w-80 m-auto py-6 px-3 rounded-xl shadow-xl'>
+        <div className='flex flex-col justify-center items-center  gap-5'>
+          <h1 className='text-center font-bold text-4xl max-md:text-2xl'>Your Profile</h1>
+          <div className='flex items-center h-50 justify-center gap-10  max-md:flex-col max-md:items-center max-md:justify-center max-md:mt-20'>
+            <div>
+              <img className='rounded-full w-30 h-30 md:w-40 md:h-40' src={user && user?.photoURL} alt="" />
+            </div>
+            <div className='space-y-4 '>
+              <p className='max-md:text-sm'><span className='font-semibold'>Name:</span> {user && user?.displayName}</p>
+              <p className='max-md:text-sm'><span className='font-semibold'>Email:</span> {user && user?.email}</p>
+              <Link to={'/userinfo/update'} className='btn btn-info max-md:text-sm'>Update Profile</Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
